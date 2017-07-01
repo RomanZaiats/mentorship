de49d20ea97d7927eeb7ab16ee63c50dd04346e8
Roman Zajats <romzayac@gmail.com>
2017-07-01 17:25:34 +0300

    add multithreading example

---
+++ Mentorship/Multithreading/MainForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace multithreading
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.start2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(12, 12);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "start1";
+            this.start.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start1_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 71);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(442, 23);
+            this.progressBar.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 166);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(442, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(213, 12);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "stopAll";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(12, 106);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(442, 23);
+            this.progressBar2.TabIndex = 4;
+            // 
+            // start2
+            // 
+            this.start2.Location = new System.Drawing.Point(109, 12);
+            this.start2.Name = "start2";
+            this.start2.Size = new System.Drawing.Size(75, 23);
+            this.start2.TabIndex = 5;
+            this.start2.Text = "start2";
+            this.start2.UseVisualStyleBackColor = true;
+            this.start2.Click += new System.EventHandler(this.start2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 198);
+            this.Controls.Add(this.start2);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.start);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Button start2;
+    }
+}
+
---
+++ Mentorship/Multithreading/MainForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multithreading
+{
+    public partial class MainForm : Form
+    {
+        private delegate void EventHandle();
+        private bool stop = false;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void start1_Click(object sender, EventArgs e)
+        {
+            Thread _progressBar1Thread = new Thread(GoProgressBar);
+            _progressBar1Thread.Start();
+        }
+
+        private void GoProgressBar()
+        {
+            Invoke(new EventHandle(() =>
+            {
+                progressBar.Style = ProgressBarStyle.Marquee;
+                progressBar.MarqueeAnimationSpeed = 6;
+            }));
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            FinishAll();
+        }
+
+        private void start2_Click(object sender, EventArgs e)
+        {
+            Thread _progressBar2Thread = new Thread(new ThreadStart(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (stop == true)
+                    {
+                        progressBar2.BeginInvoke(new Action(() => progressBar2.Value = 0));
+                        stop = false;
+                        return;
+                    }
+
+                    Thread.Sleep(40);
+                    progressBar2.BeginInvoke(new Action(() => progressBar2.Value = i));
+                }
+                Thread.Sleep(2000);
+                progressBar2.BeginInvoke(new Action(() =>
+                {
+                    FinishAll();
+                    stop = false;
+                }));
+            }));
+
+            _progressBar2Thread.Start();
+        }
+
+        private void FinishAll()
+        {
+            stop = true;
+            progressBar.Style = ProgressBarStyle.Continuous;
+            progressBar.MarqueeAnimationSpeed = 0;
+            progressBar2.Value = 0;
+        }
+    }
+}
